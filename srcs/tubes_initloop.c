c630a701fe96a78ff49a4c2cb504fc0ff5a65f83
Jean-Judicael DAUFIN <jdaufin@student.42.fr>
2017-10-16 15:48:39 +0200

    Fixed : comments endend the tubes definition loop

--- srcs/tubes_initloop.c
+++ srcs/tubes_initloop.c
@@ -6,7 +6,7 @@
 /*   By: jdaufin <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/10/13 16:04:17 by jdaufin           #+#    #+#             */
-/*   Updated: 2017/10/13 16:11:11 by jdaufin          ###   ########.fr       */
+/*   Updated: 2017/10/16 15:47:00 by jdaufin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,9 +16,11 @@ void	tubes_initloop(t_list **roomlist, t_list *input)
 {
 	if (!(roomlist && input))
 		return ;
-	while (input && is_tube((char *)(input->content)))
+	while (input && (is_tube((char *)(input->content)) ||\
+				is_comment((char *)(input->content))))
 	{
-		if (!add_tube(roomlist, (char *)(input->content)))
+		if (!is_comment((char *)(input->content)) && !add_tube(roomlist,\
+					(char *)(input->content)))
 			ft_error(PARSING,"Warning : edges setting interrupted" , 0);
 		input = input->next;
 	}
